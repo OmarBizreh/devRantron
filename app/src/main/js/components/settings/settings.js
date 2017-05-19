9b980c279f6ef90dfc25c827aa165bc227aa8ac8
Tahnik Mustasin <tahnik@live.co.uk>
2017-05-19 20:10:24 +0100

    change settings component

--- app/src/main/js/components/settings/settings.js
+++ app/src/main/js/components/settings/settings.js
@@ -1,25 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import SettingsEdit from './settings_edit';
 import SettingsPreview from './settings_preview';
 
-const Settings = (props) => {
-  const { theme, changeBackground } = props;
-  const previewTheme = theme;
-  return (
-    <div
-      className="settings_container"
-      style={{ background: previewTheme.backgroundColor }}
-    >
-      <SettingsEdit
-        theme={theme}
-        changeBackground={changeBackground}
-        previewTheme={previewTheme}
-      />
-      <SettingsPreview theme={theme} />
-    </div>
-  );
-};
+class Settings extends Component {
+  constructor() {
+    super();
+    this.state = {
+      theme: {
+        name: 'Dark Theme',
+        backgroundColor: '#54556E',
+        rant_card: {
+          backgroundColor: '#40415A',
+          color: 'white',
+        },
+        comment_card: {
+          backgroundColor: '#40415A',
+          color: 'white',
+        },
+        column: {
+          backgroundColor: '#54556E',
+          width: '27',
+        },
+        user_badge: {
+          details_back: '#54556E',
+        },
+      },
+    };
+  }
+  componentWillMount() {
+    const { theme } = this.props;
+    this.setState({
+      theme: { backgroundColor: theme.backgroundColor },
+    });
+  }
+  changeBackgroundPrev(value) {
+    this.setState({
+      theme: { backgroundColor: value },
+    });
+  }
+  render() {
+    const { theme, changeBackground } = this.props;
+    return (
+      <div
+        className="settings_container"
+      >
+        <SettingsEdit
+          theme={theme}
+          changeBackground={changeBackground}
+          changeBackgroundPrev={value => this.changeBackgroundPrev(value)}
+        />
+        <SettingsPreview theme={theme} previewTheme={this.state.theme} />
+      </div>
+    );
+  }
+}
 
 Settings.propTypes = {
   theme: PropTypes.object.isRequired,
